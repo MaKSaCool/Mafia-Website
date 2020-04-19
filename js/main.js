64d29d96b9e122a6478f6704839b55cf7dd9d352
MaKSa <tenensjkyj@gmail.com>
2020-04-19 18:20:26 +0300

    Minor fixes. Add favicon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let cards = document.querySelectorAll(".heroes_wrapper_item");
-const body = document.querySelector(".body");
+const body = document.getElementsByTagName('body')[0];
 cards.forEach((card) => {
     card.addEventListener("click", function() {
         card.classList.toggle("active");
@@ -11,7 +11,6 @@ window.addEventListener('DOMContentLoaded', () => {
     menuItem = document.querySelectorAll('.menu_item'),
     hamburger = document.querySelector('.hamburger');  
     let width = window.innerWidth || document.documentElement.clientWidth  || document.body.clientWidth;
-    console.log(body);
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
