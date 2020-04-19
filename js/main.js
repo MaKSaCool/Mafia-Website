1618b3c634e9466afae40bd34d84af2832a0b100
MaKSa <tenensjkyj@gmail.com>
2020-04-19 17:39:58 +0300

    Alfa 1.0

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let cards = document.querySelectorAll(".heroes_wrapper_item");
+
+cards.forEach((card) => {
+    card.addEventListener("click", function() {
+        card.classList.toggle("active");
+    });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger'),
+    body = document.querySelectorAll(".body"),
+    width = window.innerWidth || document.documentElement.clientWidth  || document.body.clientWidth;
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+        if (width <= 767 )  {
+            body.classList.toggle('no_scroll');}
+        
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+            if (width<= 767)  {
+                body.classList.toggle('no_scroll');}
+        });
+    });
+});
+
